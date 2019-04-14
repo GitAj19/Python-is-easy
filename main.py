5175d4873008b9c393b7963c26c58076320fd949
GitAj19 <ajmaluop@gmail.com>
2019-04-14 23:20:34 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+"""Python Homework #1 - Understanding Variables"""
+
+#Name of the song
+sSongName = "Am I Wrong"
+#Type of the song
+sGenere = "Pop"
+#Release date
+sReleasedDate = "April 12, 2013"
+#Album
+sAlbum = "Black Star Elephant"
+#Link to the song
+sUrl = "https://www.youtube.com/watch?v=bg1sT4ILG0w"
+#Singer
+sArtist = "Nico & Vinz"
+#Composers
+sSongWriters = "William Wiik Larsen, Nico Sereba, Vincent Dery, Abdoulie Jallow"
+#Producer
+sProducers = "William Wiik Larsen"
+#Length of the song in mintues
+nDuration = 4.07
+#Nominations
+sNominations = "Spellemann Award for Hit of the Year - 2014\n\t\t\t\tSoul Train Music Award for Best International Performance - 2014\n\t\t\t\tBillboard Music Award for Top Radio Song - 2015"
+
+# Print details
+print("Song\t\t: \t"+sSongName)
+print("Genere\t\t: \t"+sGenere)
+print("Released Date\t: \t"+sReleasedDate)
+print("Album\t\t: \t"+sAlbum)
+print("Url\t\t: \t"+sUrl)
+print("Artist\t\t: \t"+sArtist)
+print("Song Writers\t: \t"+sSongWriters)
+print("Producer(s)\t: \t"+sProducers)
+print("Duration(mins)\t: \t"+str(nDuration))
+print("Nominations\t: \t"+sNominations)
