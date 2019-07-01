970c7285afcc9c9a4dc0d99304d4ea272f51a3fb
GitAj19 <ajmaluop@gmail.com>
2019-07-01 08:23:20 +0530

    Python is Easy - Homework #2

---
+++ functions.py
@@ -0,0 +1,19 @@
+#Nominations
+def nominations() :
+  sNominations = "Spellemann Award for Hit of the Year - 2014\n\t\t\t\tSoul Train Music Award for Best International Performance - 2014\n\t\t\t\tBillboard Music Award for Top Radio Song - 2015"
+  print("Nominations\t: \t"+sNominations)
+
+#Link to the song
+def url() :
+  sUrl = "https://www.youtube.com/watch?v=bg1sT4ILG0w"
+  return sUrl
+
+#Composers
+def composers() :
+  sSongWriters = "William Wiik Larsen, Nico Sereba, Vincent Dery, Abdoulie Jallow"
+  print("Song Writers\t: \t"+sSongWriters)
+
+
+def VideoAvailable (url) :
+  available = True
+  return available
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-"""Python Homework #1 - Understanding Variables"""
+"""Python Homework #2 - Functions"""
+
+import functions
 
 #Name of the song
 sSongName = "Am I Wrong"
@@ -8,27 +10,22 @@ sGenere = "Pop"
 sReleasedDate = "April 12, 2013"
 #Album
 sAlbum = "Black Star Elephant"
-#Link to the song
-sUrl = "https://www.youtube.com/watch?v=bg1sT4ILG0w"
 #Singer
 sArtist = "Nico & Vinz"
-#Composers
-sSongWriters = "William Wiik Larsen, Nico Sereba, Vincent Dery, Abdoulie Jallow"
 #Producer
 sProducers = "William Wiik Larsen"
 #Length of the song in mintues
 nDuration = 4.07
-#Nominations
-sNominations = "Spellemann Award for Hit of the Year - 2014\n\t\t\t\tSoul Train Music Award for Best International Performance - 2014\n\t\t\t\tBillboard Music Award for Top Radio Song - 2015"
 
 # Print details
 print("Song\t\t: \t"+sSongName)
 print("Genere\t\t: \t"+sGenere)
 print("Released Date\t: \t"+sReleasedDate)
 print("Album\t\t: \t"+sAlbum)
-print("Url\t\t: \t"+sUrl)
+print("Video Available : "+str(functions.VideoAvailable(functions.url())))
+print("Video Url\t\t: \t"+functions.url())
 print("Artist\t\t: \t"+sArtist)
-print("Song Writers\t: \t"+sSongWriters)
+functions.composers()
 print("Producer(s)\t: \t"+sProducers)
 print("Duration(mins)\t: \t"+str(nDuration))
-print("Nominations\t: \t"+sNominations)
+functions.nominations()
