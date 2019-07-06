63ae8bfbf1eacdaea6738467e0844fd39e99a100
GitAj19 <ajmaluop@gmail.com>
2019-07-07 00:23:38 +0530

    Python is Easy - Homework #3

---
+++ Homework Assignment #3: "If" Statements/functions.py
@@ -0,0 +1,6 @@
+def checkEqual(p0,p1,p2) :
+  if int(p0) == int(p1) or int(p0) == int(p2) or int(p1) == int(p2) :
+    equal = True
+  else :
+    equal = False
+  return equal
---
+++ Homework Assignment #3: "If" Statements/main.py
@@ -0,0 +1,22 @@
+"""Python Homework #3 - If Statements"""
+
+import functions
+
+# Print details
+print("Check equal numbers 9, 8, 8")
+print(functions.checkEqual(9,"8",8))
+
+print("Check equal numbers 101, 101, 450")
+print(functions.checkEqual(101, 101, 450))
+
+print("Check equal numbers 10, 1.01, 10")
+print(functions.checkEqual(10, 1.01, 10))
+
+print("Check equal numbers -1.234 1.234, -8.3")
+print(functions.checkEqual(-1.234, 1.234, -8.3))
+
+print("Check equal numbers 5.3, -54, -5.3")
+print(functions.checkEqual(5.3, -54, -5.3))
+
+print("Check equal numbers -1.1, -5.3, -1.1")
+print(functions.checkEqual(-1.1, -5.3, -1.1))
